908fdd30c940e6c5eb5b2c5c24a71d1fe9b92a77
Guilhem Raux <g.raux@icloud.com>
2023-02-03 21:53:39 +0100

    new file for running tests

---
+++ framework/utest_run.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utest_run.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/03 21:50:39 by graux             #+#    #+#             */
+/*   Updated: 2023/02/03 21:53:07 by graux            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+#include "_utest.h"
+#include <stdlib.h>
+
+t_status	utest_run(t_utest_ptr utest)
+{
+	//TODO implement
+	return (FAIL);
+}
+
+t_status	utest_run_all(t_utest_ptr ustest)
+{
+	//TODO implement
+	return (FAIL);
+}
