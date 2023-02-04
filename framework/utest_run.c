c9b9357cf28664cf9ec0b82aa94d340d2ec85b90
Guilhem Raux <g.raux@icloud.com>
2023-02-04 18:01:29 +0100

    color printing and recap message for routine

--- framework/utest_run.c
+++ framework/utest_run.c
@@ -6,12 +6,26 @@
 /*   By: graux <graux@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 21:50:39 by graux             #+#    #+#             */
-/*   Updated: 2023/02/04 17:42:19 by graux            ###   ########.fr       */
+/*   Updated: 2023/02/04 18:00:21 by graux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
+#include "color.h"
 #include <stdlib.h>
+
+static void	print_recap(size_t passed, size_t total)
+{
+	put_str("\n");
+	if (passed == total)
+		put_str(GREEN"OK "RESET);
+	else
+		put_str(RED"FAIL "RESET);
+	put_nbr(passed);
+	put_str("/");
+	put_nbr(total);
+	put_str("\n");
+}
 
 t_status	utest_run(const t_utest *utest)
 {
@@ -36,5 +50,6 @@ t_status	utest_run_routine(const t_utest *utests, size_t routine_size)
 			passed_tests++;
 		i++;
 	}
+	print_recap(passed_tests, routine_size);
 	return (status);
 }
